5ef23c772fbbd021d5c60f87bf5fc5ab09398816
xflocp <134934709+3485871248@users.noreply.github.com>
2024-07-11 11:33:09 +0800

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,14 +1,1 @@
-import win32gui
-import win32con
 
-
-def set_foreground_window():
-    hwnd = win32gui.GetForegroundWindow()
-    win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
-
-
-if __name__ == "__main__":
-    set_foreground_window()
-
-    # 这里可以放你的CLI程序的主逻辑
-    input("Press Enter to exit...")
